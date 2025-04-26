4112626eae3b236bfd6a292b5df8c74adb26c46a
DRK NET <naveenjeeva2105@gmail.com>
2025-04-26 22:21:09 +0530

    fix: update router basename and asset paths for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/relatedlab-insight-report">
         <Routes>
           {/* Main route */} 
           <Route path="/" element={<Index />} />
